1eccaa00051c0c381cf0422ddb26919c3718b8c9
bloodyrainday <bloodyrainday@gmail.com>
2024-06-11 12:07:28 +0300

    feat: add some functions to toggle popup block

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.querySelector(".btn").addEventListener("click", () => {
-  console.log("click");
+const mainButton = document.querySelector(".btn");
+const popup = document.querySelector(".overlay");
+const closePopupBtn = document.querySelector(".close-btn span");
+
+mainButton.addEventListener("click", () => {
+  popup.classList.toggle("active");
+});
+
+closePopupBtn.addEventListener("click", () => {
+  popup.classList.toggle("active");
 });
