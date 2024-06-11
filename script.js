0c9a652436bdf70f8ebad9f87859f393167f8ebc
bloodyrainday <bloodyrainday@gmail.com>
2024-06-11 12:20:52 +0300

    feat: add function to close popup section clickling on background

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const mainButton = document.querySelector(".btn");
 const popup = document.querySelector(".overlay");
 const closePopupBtn = document.querySelector(".close-btn span");
+const overlay = document.querySelector(".overlay");
 
 mainButton.addEventListener("click", () => {
   popup.classList.toggle("active");
 });
 
-closePopupBtn.addEventListener("click", () => {
+closePopupBtn.addEventListener("click", (e) => {
+  console.log(e.target);
   popup.classList.toggle("active");
+});
+
+overlay.addEventListener("click", (e) => {
+  console.log(e.target.classList.remove("active"));
 });
